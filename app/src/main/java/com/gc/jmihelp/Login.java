1523b65a70056f264e7771bb101f36c71973987c
mohammed-azeemuddin <mohammedazeemuddin98@gmail.com>
2021-02-24 22:57:07 +0530

    added signin page

--- app/src/main/java/com/gc/jmihelp/Login.java
+++ app/src/main/java/com/gc/jmihelp/Login.java
@@ -2,13 +2,30 @@ package com.gc.jmihelp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        loginBtn = findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/gc/jmihelp/Splash.java
+++ app/src/main/java/com/gc/jmihelp/Splash.java
@@ -19,7 +19,7 @@ public class Splash extends Activity {
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }finally{
-                    Intent intent = new Intent(Splash.this,MainActivity.class);
+                    Intent intent = new Intent(Splash.this,Login.class);
                     startActivity(intent);
                 }
             }
